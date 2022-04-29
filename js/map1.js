5757edeb9729aaf7b0f04e40ec6a88851bd1d30c
Austin Jenchi <ajenchi9@gmail.com>
2022-04-28 18:53:46 -0700

    map1

---
+++ js/map1.js
@@ -0,0 +1,104 @@
+'use strict';
+
+/* global mapboxgl */
+
+(function() {
+    const layers = [
+        '0-9',
+        '10-19',
+        '20-49',
+        '50-99',
+        '100-199',
+        '200-499',
+        '500-999',
+        '1000 and more'
+    ];
+    const colors = [
+        '#FFEDA070',
+        '#FED97670',
+        '#FEB24C70',
+        '#FD8D3C70',
+        '#FC4E2A70',
+        '#E31A1C70',
+        '#BD002670',
+        '#80002670'
+    ];
+    mapboxgl.accessToken = 'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/dark-v10', // style URL
+        center: [-98.5795, 39.8283], // starting position [lng, lat]
+        zoom: 4, // starting zoom
+        projection: 'albers'
+    });
+
+    map.on('load', () => {
+        map.addSource('cov19-rates', {
+            type: 'geojson',
+            data: 'assets/us-covid-2020-rates.geojson'
+        });
+        map.addLayer({
+            'id': 'rates-layer',
+            'type': 'fill',
+            'source': 'cov19-rates',
+            'paint': {
+                'fill-color': [
+                    'step',
+                    ['get', 'rates'],
+                    '#FFEDA0',   // stop_output_0
+                    10,          // stop_input_0
+                    '#FED976',   // stop_output_1
+                    20,          // stop_input_1
+                    '#FEB24C',   // stop_output_2
+                    50,          // stop_input_2
+                    '#FD8D3C',   // stop_output_3
+                    100,         // stop_input_3
+                    '#FC4E2A',   // stop_output_4
+                    200,         // stop_input_4
+                    '#E31A1C',   // stop_output_5
+                    500,         // stop_input_5
+                    '#BD0026',   // stop_output_6
+                    1000,        // stop_input_6
+                    "#800026"    // stop_output_7
+                ],
+                'fill-outline-color': '#BBBBBB',
+                'fill-opacity': 0.7,
+            }
+        });
+    });
+
+    map.on('mousemove', ({point}) => {
+        const state = map.queryRenderedFeatures(point, {
+            layers: ['rates-layer']
+        });
+        document.getElementById('text-description').innerHTML = state.length ?
+            `<h3>${state[0].properties.county} County in ${state[0].properties.state}</h3>
+            <p>
+                <em>
+                    <strong>${state[0].properties.pop18}</strong> population and
+                    <strong>${state[0].properties.cases}</strong> cases
+                </em>
+            </p>` : '<p>Hover over a county!</p>';
+    });
+
+    const legend = document.getElementById('legend');
+
+    layers.forEach((layer, i) => {
+        const color = colors[i];
+        const item = document.createElement('div');
+        const key = document.createElement('span');
+        key.className = 'legend-key';
+        key.style.backgroundColor = color;
+    
+        const value = document.createElement('span');
+        value.innerHTML = `${layer}`;
+        item.appendChild(key);
+        item.appendChild(value);
+        legend.appendChild(item);
+    });
+
+    // const source =
+    //     '<p style="text-align: right; font-size:10pt">Source: <a href="https://earthquake.usgs.gov/earthquakes/">USGS</a></p>';
+
+    // legend.innerHTML = source;
+})();
